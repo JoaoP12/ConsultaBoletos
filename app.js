1386cc7323517df4c40c6c125421d52528820daa
Joao Pedro Lopes <janesgbs@gmail.com>
2022-06-08 18:31:57 -0400

    chore: separada a configuração do server de sua inicialização

--- app.js
+++ app.js
@@ -5,8 +5,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3000,  function () {
-    console.log("Server listening at http://localhost:3000");
-});
-
 module.exports = app;
